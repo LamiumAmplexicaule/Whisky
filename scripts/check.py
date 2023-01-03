06cc7b557f0411d7a19be29b6e005338a1f53870
LamiumAmplexicaule <47999601+LamiumAmplexicaule@users.noreply.github.com>
2023-01-03 23:23:44 +0900

    Add duplicate detection

--- scripts/check.py
+++ scripts/check.py
@@ -1,15 +1,21 @@
 import csv
 import glob
+from collections import defaultdict
 
 VALUES_LENGTH = 6
+JAN_CODE_POSITION = 1
 
 
 def main():
     csv_paths = glob.glob('../csv/*.csv', recursive=True)
+    jan_codes = defaultdict(int)
     for csv_path in csv_paths:
         with open(csv_path) as f:
             reader = csv.reader(f)
             for row in reader:
+                jan_codes[row[JAN_CODE_POSITION]] += 1
+                if jan_codes[row[JAN_CODE_POSITION]] > 1:
+                    print(f"{row} has a duplicate JAN code.")
                 if len(row) != VALUES_LENGTH:
                     print(f"{row} is not the specified length {VALUES_LENGTH}.")
 
